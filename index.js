45ca8124e7dc9a9c5d169598ea422e5533a25636
asifikbal09 <asif.ikbal662@gmail.com>
2023-09-04 20:35:47 +0600

    vercel diploy

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-//
+
+
+
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers?.authorization;
   if (!authorization) {
     res.status(401).send({ error: true, message: "Unauthorized access" });
   }
-  const token = authorization.split(" ")[1];
+  const token = authorization?.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
@@ -103,7 +105,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/cards", verifyJWT, async (req, res) => {
+    app.get("/cards",async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
